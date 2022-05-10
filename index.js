d4a5ec034dfec3ebdd96cfdb92b37eb8c4e1da2b
Zane Stock <zanestock3@gmail.com>
2022-05-10 07:41:40 -0500

    commit tree e37aaccf331af4df4f71b26392e06ec80032ff9c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Cat {
+    constructor(name, sex, speak) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = speak;
+    }
+    sasha() {
+        return `${this.name} says ${this.speak}!`
+    }
+}
+
+class Dog {
+    constructor(name, sex, speak) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = speak;
+    }
+}
+
+class Bird {
+    constructor(name, sex, speak) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = speak;
+    }
+}
+
+let sasha = new Cat("Sasha", "female", "meow")
+
+
+
